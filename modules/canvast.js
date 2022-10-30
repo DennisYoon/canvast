5f433942b42525f0da726775c4f81c2adb254cd6
DennisYun <2008dennisyun@gmail.com>
2022-10-30 20:12:02 +0900

    wait method added. remove method added. auto clean canvas added.

--- modules/canvast.js
+++ modules/canvast.js
@@ -1,49 +1,55 @@
 class Canvast {
-  /**
-   * @param {string} canvasID 
-   */
   constructor(canvasID) {
     this.canvas = document.getElementById(canvasID).getContext("2d");
-    this.span = 0; // 나누기 60 = 초
+    this.framesec = 0; // 나누기 60 = 초
     this.objects = {};
+    this.taskPause = [];
+    this.presentRunningTaskIdx = 0;
   }
 
-  /**
-   * @param {string} name object name
-   * @param {string} content object content
-   */
   add(name, content) {
-    const paramExist = [name, content].every(v => v !== undefined);
-    const uniqueKey = !(name in this.objects);
-    if (paramExist) {
-      if (uniqueKey) {
-        this.objects[name] = content;
-      } else {
-        console.error(`CANVAST.JS : added exist object '${name}'`);
-      }
-    } else {
-      console.error(`CANVAST.JS : added undefined name or content or both`);
-    }
+    this.objects[name] = content;
   }
 
-  /**
-   * @param {(ctx: HTMLCanvasElement) => void} callback 
-   */
+  remove(name) {
+    delete this.objects[name];
+  }
+
+  wait(sec) {
+    return {
+      framesec: () => {
+        this.taskPause[this.presentRunningTaskIdx] = sec;
+      },
+      second: () => {
+        this.taskPause[this.presentRunningTaskIdx] = sec * 60;
+      }
+    };
+  }
+
   classic(callback) {
     callback(this.canvas);
   }
 
-  /**
-   * @param {Generator<void, void, unknown>[]} tasksNoCall generator functions
-   */
-  show(tasksNoCall) {
+  show(...tasksNoCallSpread) {
+    const tasksNoCall = [...tasksNoCallSpread];
     return new Promise(res => {
       let tasks = tasksNoCall.map(v => v());
       let taskDown = Array(tasks.length).fill(false);
+      this.taskPause = Array(tasks.length).fill(0);
+
       function animate() {
-        console.log(taskDown, this.span);
-        tasks.forEach((t, i) => t.next().done ? taskDown[i] = true : void(0));
-        this.span++;
+        const self = this;
+        this.canvas.fillStyle = "white";
+        this.canvas.fillRect(0, 0, 400, 400);
+        tasks.forEach((task, i) => {
+          self.presentRunningTaskIdx = i;
+          if (this.taskPause[i] > 0) {
+            this.taskPause[i]--;
+          } else if (task.next().done) {
+            taskDown[i] = true;
+          }
+        });
+        this.framesec++;
         if (taskDown.every(v => v)) {
           res();
           return;
@@ -55,22 +61,19 @@ class Canvast {
   }
 }
 
+
+
 const c = new Canvast("canvas1");
 
 function* task1() {
-  yield c.add("hello world");
-  yield console.log("wow world");
-  yield c.classic((ctx) => {
-    ctx.rect(20, 20, 150, 100);
-    ctx.stroke();
-  })
+  yield c.wait(2).second();
+  for (let i = 0; i < 100; i++) {
+    yield c.classic((ctx) => {
+      ctx.rect(20, 20, 150, 100);
+      ctx.stroke();
+    });
+  }
+  
 }
 
-function* task2() {
-  yield c.add("hello world", "there you are");
-  yield console.log("good morning world", "there use are");
-}
-
-c.show([task1, task2]).then(() => {
-  console.log(c.objects);
-});
+c.show(task1);
