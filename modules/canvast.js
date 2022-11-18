3524bdd7e4920e6e2a804bb312cc578d09ccc09a
DennisYun <2008dennisyun@gmail.com>
2022-11-18 18:42:10 +0900

    rect object

--- modules/canvast.js
+++ modules/canvast.js
@@ -4,8 +4,13 @@ class Canvast {
   taskPause = [];
   nowTaskIdx = 0;
 
-  constructor(element) {
+  constructor(element, properties = {
+    canvasWidth: 0,
+    canvasHeight: 0,
+    clearScreenWhenExecutionFinished: true
+  }) {
     this.canvas = element.getContext("2d");
+    this.properties = properties;
   }
   
   add(name, content) {
@@ -14,6 +19,10 @@ class Canvast {
 
   remove(name) {
     delete this.objects[name];
+  }
+
+  o(name) {
+    return this.objects[name];
   }
 
   wait4(sec) {
@@ -32,9 +41,16 @@ class Canvast {
       this.taskPause = Array(tasks.length).fill(0);
 
       function animate() {
+        if (taskDown.every(v => v)) {
+          if (this.properties.clearScreenWhenExecutionFinished) {
+            this.clearScreen();
+          }
+          res();
+          return;
+        }
+
         const self = this;
-        this.canvas.fillStyle = "white";
-        this.canvas.fillRect(0, 0, 400, 400);
+        this.clearScreen();
         tasks.forEach((task, i) => {
           self.nowTaskIdx = i;
           if (this.taskPause[i] > 0) {
@@ -44,12 +60,13 @@ class Canvast {
           }
         });
         this.fs++;
-        if (taskDown.every(v => v)) {
-          res();
-          return;
+
+        for (let object of Object.values(this.objects)) {
+          object.showAt(this.canvas);
         }
         requestAnimationFrame(animate.bind(this));
       }
+
       animate.call(this);
     });
   }
@@ -61,27 +78,82 @@ class Canvast {
     g.wait4 = (sec) => this.wait4(sec);
     g.classic = (callback) => this.classic(callback);
   }
+
+  clearScreen() {
+    this.canvas.fillStyle = "white";
+    this.canvas.fillRect(
+      0, 0,
+      this.properties.canvasWidth, this.properties.canvasHeight
+    );
+  }
+}
+
+class Obj {
+  att = {
+    visible: true,
+    fillColor: "red",
+    strokeColor: "red"
+  }
+}
+
+class Rect extends Obj {
+  constructor() {
+    super();
+    this.att = {
+      ...this.att,
+      width: 10,
+      height: 20,
+      x: 10,
+      y: 10
+    };
+  }
+
+  showAt(ctx) {
+    const atts = this.att;
+    ctx.fillStyle = atts.fillColor;
+    ctx.strokeStyle = atts.strokeColor;
+    ctx.fillRect(
+      atts.x, atts.y,
+      atts.x + atts.width, atts.y + atts.height
+    );
+  }
+
+  addWidth(number) {
+    this.att.width += number;
+  }
+
+  setWidth(number) {
+    this.att.width = number;
+  }
+
+  addHeight(number) {
+    this.att.height += number;
+  }
+
+  setHeight(number) {
+    this.att.height = number;
+  }
 }
 
 function canvast(e) {
-  return new Canvast(e)
+  return new Canvast(e);
 };
 
 
+/** **/
 const canvas = document.querySelector("#canvas1");
-const c = canvast(canvas);
-c.cout();
+const c = new Canvast(canvas, {
+  canvasWidth: 400,
+  canvasHeight: 400,
+  clearScreenWhenExecutionFinished: false
+});
 
-function* task1() {
-  yield wait4(20);
-  console.log("waited");
+function* t1() {
+  yield c.add("rect1", new Rect());
   for (let i = 0; i < 100; i++) {
-    yield classic((ctx) => {
-      ctx.rect(20, 20, 150, 100);
-      ctx.stroke();
-    });
+    yield c.o("rect1").addWidth(1);
   }
-  
+  yield c.o("rect1").setWidth(10);
 }
 
-c.show(task1);
+c.show(t1);
